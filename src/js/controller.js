28728979849fa3fbacf170ef0417c616c32be2f7
= <miradorzander@gmail.com>
2024-07-02 20:18:37 +0800

    merge to main

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,21 +2,4 @@
 
 ///////////////////////////////////////
 
-const showRecipe = async () => {
-  try {
-    const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
-    );
-    const data = await res.json();
-
-    console.log(res);
-  } catch (err) {
-    alert(err);
-  }
-};
-
-fetch(
-  'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
-);
-console.log('asd');
-showRecipe();
+console.log('TEST');
